362cf1eefc4cc169a8586aca4429ad144f21e7cc
MathiasM-Git <80817933+MathiasM-Git@users.noreply.github.com>
2023-06-07 23:26:37 +0200

    wow

--- src/main/java/fr/yncrea/cin3/ths/main.java
+++ src/main/java/fr/yncrea/cin3/ths/main.java
@@ -12,56 +12,48 @@ import fr.yncrea.cin3.ths.FFT.FFTCplx;
 
 import java.util.Arrays;
 
+import static java.util.Arrays.copyOfRange;
+
 public class main {
+
+
 
     public static void main(String[] args) {
 
         //tableau de string contenant les fichiers audio
-        String[] fichiers = {"Canard_2.wav", "Chouette_Hulotte_1.wav"};
-        float[][] entree = creationEntree(fichiers);
-        float[] sortie = creationSortie(fichiers);
+        String[] fichiers = {"Canard_2.wav", "Canard_1.wav","Canard_3.wav", "Chouette_Hulotte_1.wav","Chouette_Effraie.wav"};
 
-        final iNeurone n = new NeuroneSigmoide(entree[0].length);
+        final Neurone n = apprentissageNeuroneSigmoide(fichiers, 3  );
 
-        System.out.println("Apprentissage…");
-        System.out.println("Nombre de tours : " + n.apprentissage(entree, sortie));
-
-        final Neurone vueNeurone = (Neurone) n;
-        System.out.print("Synapses : ");
-        for (final float f : vueNeurone.synapses())
-            System.out.print(f + " ");
-        System.out.print("\nBiais : ");
-        System.out.println(vueNeurone.biais());
-
-        // On affiche chaque cas appris
-        for (int i = 0; i < entree.length; ++i) {
-            // Pour une entrée donnée
-            final float[] entrees = entree[i];
-            // On met à jour la sortie du neurone
-            n.metAJour(entrees);
-            // On affiche cette sortie
-            System.out.println("Entree " + i + " : " + n.sortie());
-        }
 
         System.out.println("Test de la fonction de reconnaissance juste");
         String[] fichierTest = {"Canard_3.wav"};
         float[][] sonTest = creationEntree(fichierTest);
-        for (int i = 0; i < sonTest.length; ++i) {
-            n.metAJour(sonTest[i]);
-            System.out.println("Entree " + i + " : " + n.sortie());
+        float total = 0;
+        for (float[] floats : sonTest) {
+            n.metAJour(floats);
+            total += (Math.round(n.sortie()) == 1 ? 1 : 0);
         }
+        System.out.println("Moyenne : " + total / sonTest.length);
 
         System.out.println("Test de la fonction de reconnaissance fausse");
         String[] fichierTest2 = {"Coq_1.wav"};
+        float total2 = 0;
         float[][] sonTest2 = creationEntree(fichierTest2);
-        for (int i = 0; i < sonTest2.length; ++i) {
-            n.metAJour(sonTest2[i]);
-            System.out.println("Entree " + i + " : " + n.sortie());
+        for (float[] floats : sonTest2) {
+            n.metAJour(floats);
+            total2 += (Math.round(n.sortie()) == 0 ? 1 : 0);
         }
+        System.out.println("Moyenne : " + total2 / sonTest2.length);
 
 
     }
 
+    /**
+     * Transformation de fichiers sons en tableau comportant les modules des FFT de chaque bloc
+     * @param nomFichier nom du fichier son
+     * @return  tableau de float contenant les modules des FFT de chaque bloc
+     */
     private static float[][] sonTest(String nomFichier) {
 
         int fi = 4096;
@@ -91,6 +83,11 @@ public class main {
         return entrees;
     }
 
+    /**
+     * Création d'un tableau d'entrée pour le système d'apprentissage à partir d'un tableau de noms de fichiers
+     * @param fileNames liste contenant le nom des fichiers concernés
+     * @return un tableau d'entrée concaténant les tableaux d'entrée de chaque fichier
+     */
     public static float[][] creationEntree(String[] fileNames) {
 
         float max = 0.0f;
@@ -125,29 +122,71 @@ public class main {
         return entree;
     }
 
-    public static float[] creationSortie(String[] fileNames) {
-        float[] sortie = new float[0];
+    /**
+     * Concaténation des sorties des fichiers
+     * @param filenames liste des noms de fichiers
+     * @param nbreVrai  nombre de fichiers correspondant à un résultat vrai pour le neurone
+     * @return tableau de float contenant les sorties des fichiers
+     */
+    public static float[] concatSorties(String[] filenames, int nbreVrai){
+        String[] sortieV = copyOfRange(filenames, 0, nbreVrai);
+        String[] sortieF = copyOfRange(filenames, nbreVrai, filenames.length);
 
-        float[][] entree1 = sonTest(fileNames[0]);
-        float[] sortie1 = new float[entree1.length];
-        Arrays.fill(sortie1, 1);
+        float[] sortieVout = creationSortie(sortieV, 1);
+        float[] sortieFout = creationSortie(sortieF, 0);
 
-        //remplir un tableau de sortie avec des 0 pour tous les autres fichiers en prenant la meme methode
-        for (int i = 0; i < fileNames.length; ++i) {
-            if (i == 0) {
-                sortie = sortie1;
-            } else {
-                float[][] entreeTemp = sonTest(fileNames[i]);
-                float[] sortieTemp = new float[entreeTemp.length];
-                Arrays.fill(sortieTemp, 0);
-                float[] sortieTemp2 = new float[sortie.length + sortieTemp.length];
-                System.arraycopy(sortie, 0, sortieTemp2, 0, sortie.length);
-                System.arraycopy(sortieTemp, 0, sortieTemp2, sortie.length, sortieTemp.length);
-                sortie = sortieTemp2;
-            }
-        }
+        //concatenation des deux tableaux
+        float[] sortie = new float[sortieVout.length + sortieFout.length];
+        System.arraycopy(sortieVout, 0, sortie, 0, sortieVout.length);
+        System.arraycopy(sortieFout, 0, sortie, sortieVout.length, sortieFout.length);
 
         return sortie;
+    }
+
+    /**
+     * Création d'un tableau de sortie pour le système d'apprentissage à partir d'un tableau de noms de fichiers
+     * @param fileNames liste contenant le nom des fichiers concernés
+     * @param valeur valeur de sortie
+     * @return un tableau de sortie concaténant les tableaux de sortie de chaque fichier
+     */
+    public static float[] creationSortie(String[] fileNames, int valeur) {
+        float[] sortieV = new float[0];
+
+        for (String fileName : fileNames) {
+            float[][] entreeTemp = sonTest(fileName);
+            float[] sortieTemp = new float[entreeTemp.length];
+            Arrays.fill(sortieTemp, valeur);
+            float[] sortieTemp2 = new float[sortieV.length + sortieTemp.length];
+            System.arraycopy(sortieV, 0, sortieTemp2, 0, sortieV.length);
+            System.arraycopy(sortieTemp, 0, sortieTemp2, sortieV.length, sortieTemp.length);
+            sortieV = sortieTemp2;
+        }
+        return sortieV;
+    }
+
+    public static Neurone apprentissageNeuroneSigmoide(String[] fileNames, int nbreVrai){
+        float[][] entree = creationEntree(fileNames);
+        float[] sortie = concatSorties(fileNames, nbreVrai);
+
+        final Neurone n = new NeuroneSigmoide(entree[0].length);
+
+        System.out.println("Apprentissage…");
+        System.out.println("Nombre de tours : " + n.apprentissage(entree, sortie));
+
+        System.out.print("Synapses : ");
+        for (final float f : n.synapses())
+            System.out.print(f + " ");
+        System.out.print("\nBiais : ");
+        System.out.println(n.biais());
+
+        // On affiche chaque cas appris
+        for (final float[] entrees : entree) {
+            // Pour une entrée donnée
+            // On met à jour la sortie du neurone
+            n.metAJour(entrees);
+            // On affiche cette sortie
+        }
+        return n;
     }
 
 }
